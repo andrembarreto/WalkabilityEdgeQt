ec8da30db53f864786f3a80c21b043678e8f1ed6
Andre <andremoratob@gmail.com>
2026-09-05 21:51:27 -0300

    refactor: extracted loadJson function and added early returns to clean appdata module

--- src/infra/data/appdata.cpp
+++ src/infra/data/appdata.cpp
@@ -9,13 +9,13 @@
 
 namespace appdata {
 
-QList<JourneyDimensionViewModel*> loadData()
+static QJsonObject loadJson(const char* dataFile)
 {
-    QFile file(":/data/dimensions-and-events.json");
+    QFile file(dataFile);
     if(!file.open(QIODevice::ReadOnly))
     {
-        qWarning() << "could not open dimensions-and-events.json for reading";
-        return {};
+        qWarning() << "could not open" << dataFile << "for reading";
+        return QJsonObject();
     }
 
     const QByteArray data = file.readAll();
@@ -24,40 +24,47 @@ QList<JourneyDimensionViewModel*> loadData()
 
     if(document.isNull())
     {
-        qWarning() << "dimensions-and-events.json: bad format";
+        qWarning() << dataFile << ": not JSON";
+        return QJsonObject();
+    }
+
+    return document.object();
+}
+
+QList<JourneyDimensionViewModel*> loadData()
+{
+    QJsonObject object = loadJson(":/data/dimensions-and-events.json");
+    QJsonValue dimensions = object.value("dimensions");
+
+    if(dimensions.isUndefined() || !dimensions.isArray())
+    {
+        qWarning() << "missing or malformed dimensions in data file";
         return {};
     }
 
-    QJsonObject object = document.object();
-    QJsonValue dimensions = object.value("dimensions");
-    if(dimensions.isArray())
+    QList<JourneyDimensionViewModel*> dimensionList;
+    for(const auto& dimensionRef: dimensions.toArray())
     {
-        QList<JourneyDimensionViewModel*> dimensionList;
-        for(const auto& dimensionRef: dimensions.toArray())
-        {
-            QJsonObject dimension = dimensionRef.toObject();
-            int id = dimension.value("id").toInt();
-            QString name = dimension.value("name").toString();
-            QString icon = dimension.value("icon").toString();
-            QJsonValue events = dimension.value("events");
-            if(events.isArray())
-            {
-                QList<JourneyEventViewModel*> eventList;
-                for(const auto& eventRef: events.toArray())
-                {
-                    QJsonObject event = eventRef.toObject();
-                    int id = event.value("id").toInt();
-                    QString name = event.value("name").toString();
-                    eventList.append(new JourneyEventViewModel(id, name, qApp));
-                }
-                dimensionList.append(new JourneyDimensionViewModel(id, icon, name, eventList, qApp));
-            }
-        }
-        return dimensionList;
-    }
+        QJsonObject dimension = dimensionRef.toObject();
+        int id = dimension.value("id").toInt();
+        QString name = dimension.value("name").toString();
+        QString icon = dimension.value("icon").toString();
+        QJsonValue events = dimension.value("events");
 
-    qWarning() << "dimensions-and-events.json: bad format";
-    return {};
+        if(!events.isUndefined() && events.isArray())
+        {
+            QList<JourneyEventViewModel*> eventList;
+            for(const auto& eventRef: events.toArray())
+            {
+                QJsonObject event = eventRef.toObject();
+                int id = event.value("id").toInt();
+                QString name = event.value("name").toString();
+                eventList.append(new JourneyEventViewModel(id, name, qApp));
+            }
+            dimensionList.append(new JourneyDimensionViewModel(id, icon, name, eventList, qApp));
+        }
+    }
+    return dimensionList;
 }
 
 }
